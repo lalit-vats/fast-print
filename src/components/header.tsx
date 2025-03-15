d39a2a34a21d22483f00e71a231b88cb95851177
lalit-vats <117825355+lalit-vats@users.noreply.github.com>
2025-03-15 19:46:09 +0530

    created header and complete setup

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,24 +8,138 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import Logo from "../../public/Images/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import { navLinks } from "@/lib/utils";
+import { serviceLink } from "@/lib/utils";
+import { Menu } from "lucide-react"
+
+
+interface ServiceProp {
+  name: string;
+  href: string;
+}
+interface NavProp {
+  name: string;
+  href: string;
+}
 
 const Header = () => {
   return (
-    <div>
-        
-      {/* <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Sevices</NavigationMenuTrigger>
-            <NavigationMenuContent className=" flex flex-col p-2 space-y-2 ">
-              <NavigationMenuLink className="w-[100px]">
-                Link
-              </NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu> */}
-      <div>Header</div>
+    <div className="flex justify-between items-center px-4 py-1.5 shadow-md">
+      <Image src={Logo} alt="Logo" height={100} width={200} />
+      <div className="hidden md:flex sm:block">
+      <div className=" p-2 font-medium flex">
+        {navLinks.map((link: NavProp) => {
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className="p-2 text-[#EF4136] hover:text-gray-500"
+            >
+              {link.name === "Service" ? (
+                <div>
+                  <NavigationMenu>
+                    <NavigationMenuList>
+                      <NavigationMenuItem>
+                        <NavigationMenuTrigger className="font-medium hover:text-gray-500">
+                          Sevices
+                        </NavigationMenuTrigger>
+                        <NavigationMenuContent className=" ">
+                          <NavigationMenuLink className="w-[150px] flex flex-col p-2 space-y-2  font-medium">
+                            {serviceLink.map((link: ServiceProp) => {
+                              return (
+                                <Link
+                                  key={link.name}
+                                  href={link.href}
+                                  className="p-0.5 text-[#EF4136] hover:text-gray-500"
+                                >
+                                  {link.name}
+                                </Link>
+                              );
+                            })}
+                          </NavigationMenuLink>
+                        </NavigationMenuContent>
+                      </NavigationMenuItem>
+                    </NavigationMenuList>
+                  </NavigationMenu>
+                </div>
+              ) : (
+                link.name
+              )}
+            </Link>
+          );
+        })}
+      </div>
+      </div>
+      <div className="md:hidden sm:block">
+      <Sheet>
+        <SheetTrigger>
+          <Menu size={24} />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            {/* <SheetTitle>Are you absolutely sure?</SheetTitle> */}
+            {/* <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription> */}
+            <div className=" p-2 font-medium flex flex-col">
+        {navLinks.map((link: NavProp) => {
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className="p-2 text-[#EF4136] hover:text-gray-500"
+            >
+              {link.name === "Service" ? (
+                <div>
+                  <NavigationMenu>
+                    <NavigationMenuList>
+                      <NavigationMenuItem>
+                        <NavigationMenuTrigger className="font-medium hover:text-gray-500">
+                          Sevices
+                        </NavigationMenuTrigger>
+                        <NavigationMenuContent className=" ">
+                          <NavigationMenuLink className="w-[150px] flex flex-col p-2 space-y-2  font-medium">
+                            {serviceLink.map((link: ServiceProp) => {
+                              return (
+                                <Link
+                                  key={link.name}
+                                  href={link.href}
+                                  className="p-0.5 text-[#EF4136] hover:text-gray-500"
+                                >
+                                  {link.name}
+                                </Link>
+                              );
+                            })}
+                          </NavigationMenuLink>
+                        </NavigationMenuContent>
+                      </NavigationMenuItem>
+                    </NavigationMenuList>
+                  </NavigationMenu>
+                </div>
+              ) : (
+                link.name
+              )}
+            </Link>
+          );
+        })}
+      </div>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+      </div>
     </div>
   );
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const serviceLink =[
+  {name: "Flex Print", href: "/"},
+  {name: "T-Shirt Print", href: "/"},
+  {name: "Mug Print", href: "/"},
+  {name: "Vinayl Print", href: "aboutUs"},
+  ]
+
+export const navLinks =[
+  {name: "Home", href: "/"},
+  {name: "About Us", href: "/"},
+  {name: "Service", href: "/"},
+  {name: "Contact Us", href: "/"},
+]
