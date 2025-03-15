9617c2cf7274c88b91fca3692ec153323953e819
lalit-vats <117825355+lalit-vats@users.noreply.github.com>
2025-03-15 14:59:17 +0530

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+const Header = () => {
+  return (
+    <div>
+        
+      {/* <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Sevices</NavigationMenuTrigger>
+            <NavigationMenuContent className=" flex flex-col p-2 space-y-2 ">
+              <NavigationMenuLink className="w-[100px]">
+                Link
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu> */}
+      <div>Header</div>
+    </div>
+  );
+};
+
+export default Header;
