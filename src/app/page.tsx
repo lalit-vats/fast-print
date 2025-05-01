abd2b449afe801bba35ec20981a22a2439f71b71
lalit-vats <117825355+lalit-vats@users.noreply.github.com>
2025-05-01 20:10:46 +0530

    added what we do section in home pahe and ui fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import CountUp from "@/components/CountUp";
 import ScrollVelocity from "@/components/ui/ScrollVelocity";
+import WhatWeDo from "@/components/whatWeDo";
 
 import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
 import Hero from "@/components/ui/hero";
@@ -33,7 +34,14 @@ export default function Home() {
       <div className=" top-0 z-[-2] transform bg-white bg-gradient-to-bl from-blue-200 via-yellow-50 to-pink-300 ">
         <Hero />
       </div>
+
+      {/* Services   */}
+
       <Services />
+
+
+      <WhatWeDo />
+      {/*Testimonials   */}
       <div className="container grid grid-cols-1 md:grid-cols-2  px-16 items-center ">
         <div className="p-4">
           <h4 className="text-4xl text-center font-medium md:mt-14 mb-2.5 text-[#EF4136]">
@@ -46,7 +54,7 @@ export default function Home() {
         </div>
         <AnimatedTestimonials testimonials={testimonials} />
       </div>
-      
+
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4  bg-pink-100">
         {countUpData.map((d, i) => {
           return (
@@ -73,12 +81,12 @@ export default function Home() {
           );
         })}
       </div>
-  
-  <ScrollVelocity
-    texts={['Fast Print', 'Print Services']} 
-    velocity={velocity} 
-    className="custom-scroll-text bg-gradient-to-r from-pink-600 via-purple-500 to-green-700 text-transparent bg-clip-text"
-  />
+
+      <ScrollVelocity
+        texts={['Fast Print', 'Print Services']}
+        velocity={velocity}
+        className="custom-scroll-text bg-gradient-to-r from-pink-600 via-purple-500 to-green-700 text-transparent bg-clip-text"
+      />
 
     </div>
   );
